1978f2e6998eb98085a67c729f5626de1160ccc4
Fredrik Möller <fredrik@systema.no>
2018-09-18 16:02:53 +0200

    more setup

---
+++ src/no/systema/jservices/controller/ResponseOutputterController_KOSTA.java
@@ -0,0 +1,14 @@
+package no.systema.jservices.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This controller contribute with CRUD-logic on file/table KOSTA
+ * 
+ * @author fredrikmoller
+ *
+ */
+@RestController
+public class ResponseOutputterController_KOSTA {
+
+}
