bb9335dcf800055f69a0dbf99eba80f74bd7c73e
Fredrik Möller <fredrik@systema.no>
2018-09-19 14:10:57 +0200

    All kosta

--- src/no/systema/jservices/controller/ResponseOutputterController_KOSTA.java
+++ src/no/systema/jservices/controller/ResponseOutputterController_KOSTA.java
@@ -1,6 +1,16 @@
 package no.systema.jservices.controller;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import no.systema.jservices.common.dao.KostaDao;
+import no.systema.jservices.common.dao.services.KostaDaoService;
 
 /**
  * This controller contribute with CRUD-logic on file/table KOSTA
@@ -11,4 +21,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ResponseOutputterController_KOSTA {
 
+	private static Logger logger = Logger.getLogger(ResponseOutputterController_KOSTA.class.getName());	
+
+	@Autowired
+	KostaDaoService kostaDaoDaoService;
+	
+	/**
+	 * Example :  http://localhost:8080/syjserviceskostf/kosta.do?user=SYSTEMA
+	 * @param kundnr
+	 * @param fraDato
+	 * @return
+	 */
+	@RequestMapping(path = "/kosta.do", method = RequestMethod.GET)
+	public List<KostaDao> doKosta(@RequestParam("user") String user) {
+	
+		return kostaDaoDaoService.findAll(null);
+		
+	
+	}
+	
 }
