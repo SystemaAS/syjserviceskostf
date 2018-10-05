ba94d29830cb8527d291d523b999f0072759bab6
Fredrik Möller <fredrik@systema.no>
2018-10-05 12:36:01 +0200

    Re-factor to KostaDto usage

--- src/no/systema/jservices/controller/ResponseOutputterController_KOSTA.java
+++ src/no/systema/jservices/controller/ResponseOutputterController_KOSTA.java
@@ -1,8 +1,6 @@
 package no.systema.jservices.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import no.systema.jservices.common.dao.KostaDao;
 import no.systema.jservices.common.dao.services.BridfDaoService;
-import no.systema.jservices.common.dao.services.GenericDaoServiceImpl;
 import no.systema.jservices.common.dao.services.KostaDaoService;
+import no.systema.jservices.common.dto.KostaDto;
 
 /**
  * This controller contribute with CRUD-logic on file/table KOSTA
@@ -39,21 +37,40 @@ public class ResponseOutputterController_KOSTA {
 	 */
 	@RequestMapping(path = "/kosta", method = RequestMethod.GET)
 	public List<KostaDao> getKosta(	@RequestParam(value = "user", 		required = true) String user, 
-									@RequestParam(value = "bilagsnr", 	required = false) String bilagsnr, 
-									@RequestParam(value = "innregnr", 	required = false) String innregnr,
+									@RequestParam(value = "bilagsnr", 	required = false) Integer bilagsnr, 
+									@RequestParam(value = "innregnr", 	required = false) Integer innregnr,
 									@RequestParam(value = "faktnr", 	required = false) String faktnr,
-									@RequestParam(value = "levnr", 		required = false) String levnr,
+									@RequestParam(value = "levnr", 		required = false) Integer levnr,
 									@RequestParam(value = "attkode", 	required = false) String attkode,
 									@RequestParam(value = "komment", 	required = false) String komment,						
-									@RequestParam(value = "fradato", 	required = false) Number fradato						
+									@RequestParam(value = "fradato", 	required = false) Integer fradato,
+									@RequestParam(value = "fraperaar", 	required = false) Integer fraperaar,						
+									@RequestParam(value = "frapermnd", 	required = false) Integer frapermnd,
+									@RequestParam(value = "reklamasjon",required = false) String reklamasjon,	
+									@RequestParam(value = "status",		required = false) String status,
+									@RequestParam(value = "fskode",		required = false) String fskode,
+									@RequestParam(value = "fssok",		required = false) String fssok
 								) {
 
 		logger.info("/kosta");
 		
 		checkUser(user);
 		
-		return kostaDaoDaoService.findAll(bilagsnr, innregnr, faktnr, levnr, attkode, komment, fradato);
-
+		KostaDto qDto = new KostaDto();
+		qDto.setKabnr(bilagsnr);
+		qDto.setKabnr2(innregnr);
+		qDto.setKafnr(faktnr);
+		qDto.setKalnr(levnr);
+		qDto.setKasg(attkode);
+		qDto.setKatxt(komment);
+		qDto.setKabdt(fradato);
+		qDto.setKbrekl(reklamasjon);
+		qDto.setKast(status);
+		qDto.setFskode(fskode);
+		qDto.setFssok(fssok);
+		
+		return kostaDaoDaoService.findAllComplex(qDto);
+		
 	}
 	
 	private void checkUser(String user) {
